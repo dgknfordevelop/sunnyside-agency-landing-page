6952f5cf95167e86daae64c42321d01bcca7fb8e
Doğukan Çalışkan <dogukan.engt@gmail.com>
2022-02-01 21:21:03 +0300

    main section for mobile coded

--- js/nav-switch.js
+++ js/nav-switch.js
@@ -1,15 +1,7 @@
-let navSwitch = document.querySelector(".header-switch");
-let headerNav = document.querySelector(".header-nav");
+let navSwitch = document.querySelector(".header-switch")
+let headerNav = document.querySelector(".header-nav")
 
-navSwitch.addEventListener("click", ()=>{
+navSwitch.addEventListener("click", () => {
     let navClassList = headerNav.classList;
-   
-    if(Object.values(navClassList).includes("header-nav-hidden")){
-
-        navClassList.remove("header-nav-hidden")
-
-    }else if(Object.values(navClassList).includes("header-nav-hidden") === false){
-        navClassList.add("header-nav-hidden")
-    }
-
+    navClassList.toggle("header-nav-hidden")
 })
