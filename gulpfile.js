1da114558b2c7d2adb2b65ccd1da0bb745e4bfc1
Doğukan Çalışkan <dogukan.engt@gmail.com>
2022-02-18 17:04:33 +0300

    folder structure changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,26 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const prefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
 
 function style() {
-    return gulp.src('./scss/main.scss')
+    return gulp.src('./styles/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(prefixer({
             cascade: false
         }))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./styles/*.css'))
         .pipe(browserSync.stream());
 }
 
+function minifycCSS(){
+    return gulp.src('./styles/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(gulp.dest('dist'));
+}
 
 function watch() {
 
@@ -25,6 +34,7 @@ function watch() {
     gulp.watch('./scss/**/*.scss', style); // if anything changes here it starts the 'style'.
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
+    gulp.watch('./*.css', minifycCSS)
 
 }
 
